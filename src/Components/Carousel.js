65314e96ea0d3e0005570ff758c1c1f3757678e2
boo182 <lucien.ladurie@gmail.com>
2018-04-09 00:25:02 +0200

    image carousel big fuckon fail

---
+++ src/Components/Carousel.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styles/carousel.css';
+
+export default class Carousel extends Component {
+
+  render() {
+   const { images } = this.props;
+
+
+    return (
+      <div className="carouselContainer">
+      </div>
+    )
+  }
+}
--- src/Container/Collection.js
+++ src/Container/Collection.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import PageTitle from '../Components/PageTitle';
 import data from '../assets/texts';
+import Carousel from '../Components/Carousel';
 import './styles/collection.css';
 
 export default class Collection extends Component {
     state = {
         issue: data.pages[this.props.match.params.issue],
     }
-    componentWillMount() {
-        
-    }
   render() {
-      console.log(!!this.state.issue.number);
+    console.log(this.state.issue);
     return (
       <div className="collectionContainer">
-            <PageTitle title={this.state.issue.pageTitle} number={this.state.issue.number}/>          
+          <div className="pageTitle">
+            <PageTitle title={this.state.issue.pageTitle} number={this.state.issue.number}/>
+          </div>
+            <Carousel images={this.state.issue.images}/>      
       </div>
     )
   }
